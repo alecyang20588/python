61f13e838db11ae8d74196d77bf004815c8382ea
alecyang20588 <124280571+alecyang20588@users.noreply.github.com>
2023-08-23 11:37:12 +0100

    Update browsser-open.py
    update the weblink address

--- browsser-open.py
+++ browsser-open.py
@@ -9,10 +9,10 @@ new = 2 # open in a new tab, if possible
 
 # open a public URL, in this case, the webbrowser docs
 
-url = "http://docs.python.org/library/webbrowser.html"
+url = "https://www.google.co.uk/"
 webbrowser.open(url,new=new)
 
 # open an HTML file on my own (Windows) computer
 
-url = "file://X:/MiscDev/language_links.html"
-webbrowser.open(url,new=new)
+#url = "file://X:/MiscDev/language_links.html"
+#webbrowser.open(url,new=new)
